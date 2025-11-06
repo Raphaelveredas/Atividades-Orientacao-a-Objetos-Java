6c1e4bb293528bc8e713eda97017bb1ba18ef3f6
RaphaelVeredas <Rapha6457@gmail.com>
2025-11-06 16:06:57 -0300

    Inclusão da atividade

--- atividades raphael/atividades de raphael/src/Cachorro.java
+++ atividades raphael/atividades de raphael/src/Cachorro.java
@@ -1,3 +1,30 @@
 public class Cachorro {
+    public String nome;
+    public String raca;
+    public int idade;
+
+    public Cachorro (String n, String r, int i){
+        nome = n;
+        raca = r;
+        idade = i;
+    }
+
+    //métosdos
+    public void latir(){
+        System.out.println("au au");
+    }
+
+    public String latir2(){
+        return "eu conheci um cara androgino no colegio";
+    }
+
+    public void carac(){
+        System.out.println("Nome: " + nome + "\nRaca: " + raca + "\nIdade: " + idade);
+    }
+
+    public void imprimir(){
+        carac();
+        latir();
+    }
     
 }
--- atividades raphael/atividades de raphael/src/Main.java
+++ atividades raphael/atividades de raphael/src/Main.java
@@ -1,3 +1,18 @@
 public class Main {
-    
+    public static void main(String[] args) throws Exception {
+        Cachorro c1 = new Cachorro("Bob", "Lavrador", 4);
+        Cachorro c2 = new Cachorro("Nick", "Pitbull", 2);
+        Cachorro c3 = new Cachorro("Teo", "Pincher", 3);
+
+        c1.imprimir();
+        System.out.println("\n\n\n");
+                c2.imprimir();
+        System.out.println("\n\n\n");
+
+        c3.imprimir();
+        System.out.println("\n\n\n");
+
+        String latido_principal = c1.latir2();
+        System.out.println(latido_principal);
+    }
 }
